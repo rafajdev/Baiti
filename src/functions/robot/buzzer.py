efc7a3018acc59a143a53629138d8c1cb9b95129
RafaR <rafadeoliveiramendes@gmail.com>
2024-10-06 00:07:01 -0300

    Added buzzer function logic

--- src/functions/robot/buzzer.py
+++ src/functions/robot/buzzer.py
@@ -1,3 +1,12 @@
+import pyfirmata2
+import time
 
-def beep(frequency, duration):
-   pass
+def beep():
+   board = pyfirmata2.Arduino('COM5')
+   
+   pin_buzzer = board.digital[13]
+   
+   pin_buzzer.write(1)
+   time.sleep(0.5)
+   pin_buzzer.write(0)
+   board.exit()
