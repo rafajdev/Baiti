cfc4b738f8173e673f22980328088d6abd7f25fd
RafaR <rafadeoliveiramendes@gmail.com>
2024-10-12 13:53:47 -0300

    Testing servo 1 movements

--- src/functions/robot/arm.py
+++ src/functions/robot/arm.py
@@ -1,12 +1,9 @@
 from models.Servo import Servo
 
 s1 = Servo(pin=8)
-s2 = Servo(pin=9)
 
 s1.move(0, 1.5)
 s1.move(90, 1.5)
 s1.move(180, 1.5) 
-s2.move(90, 1.5)
 
 s1.end_operation()
-s2.end_operation()
