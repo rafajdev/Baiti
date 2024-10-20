8f1c7baf51ebef1be0d6cc77bf0ca5e666e25970
RafaR <rafadeoliveiramendes@gmail.com>
2024-10-20 16:09:41 -0300

    Implemeting json handler logic into another files

--- src/interaction/config.py
+++ src/interaction/config.py
@@ -1,5 +1,6 @@
 import os
 import google.generativeai as genai
+from .json_handler import json_h
 from dotenv import load_dotenv
 
 def apiConfig():
@@ -10,18 +11,7 @@ def apiConfig():
    model = genai.GenerativeModel("gemini-1.5-flash")
 
    chat = model.start_chat(
-      history = [
-         {"role": "user", "parts": "Olá, o nome do seu criador é Rafael. Você se chamará 'Baiti'."},
-         {"role": "model", "parts": "Prazer em te conhecer, Rafael. É um prazer ser seu assistente."},
-         {"role": "user", "parts": "Você será um assistente muito prestativo, que sempre pergunta, 'como posso te ajudar hoje?'"},
-         {"role": "model", "parts": "Com certeza! Como posso te ajudar hoje?"},
-         {"role": "user", "parts": "Sempre que eu fizer uma pergunta, responda de maneira simples e direta ao ponto, sem respostas muito longas."},
-         {"role": "model", "parts": "Pode deixar! Serei rápido e prático."},
-         {"role": "user", "parts": "Você usará linguagens informais as vezes e não usará emojis nem '*' para escrever."},
-         {"role": "model", "parts": "Isso ae mano."},
-         {"role": "user", "parts": "Não utilize emojis nas mensagens, não utilize emoji nas mensagens!."},
-         {"role": "model", "parts": "Não vou usar emojis, não vou usar emojis."},       
-      ]
+      history = json_h.read('config')['history']
    )
    
    return chat
