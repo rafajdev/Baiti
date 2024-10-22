6887c2989d21cb029289537f84ec9b4756f30f24
RafaR <rafadeoliveiramendes@gmail.com>
2024-10-22 10:09:31 -0300

    Simplified the led colors logic

--- src/functions/robot/RGBLed.py
+++ src/functions/robot/RGBLed.py
@@ -23,6 +23,9 @@ class RGBLed:
     blink(red, green, blue, times, delay) : None
         Blink the LED the specified number of times with the specified delay
     """
+    import time
+
+class RGBLed:
     def __init__(self, red_pin, green_pin, blue_pin, board):
         self.red_pin = red_pin
         self.green_pin = green_pin
@@ -32,63 +35,34 @@ class RGBLed:
         self.green_led_pin = self.board.get_pin(f'd:{green_pin}:o')
         self.blue_led_pin = self.board.get_pin(f'd:{blue_pin}:o')
     
-    def on(self, red, green, blue):
+    def set_color(self, red, green, blue):
         self.red_led_pin.write(red)
         self.green_led_pin.write(green)
         self.blue_led_pin.write(blue)
     
     def off(self):
-        self.on(0, 0, 0)
+        self.set_color(0, 0, 0)
     
-    def on_talk(self, message):
-        if message.lower() == 'vermelho':
-            self.on_red()
-            return 'pass'
-        elif message.lower() == 'verde':
-            self.on_green()
-            return 'pass'
-        elif message.lower() == 'azul':
-            self.on_blue()
-            return 'pass'
-        elif message.lower() == 'amarelo':
-            self.on_yellow()
-            return 'pass'
-        elif message.lower() == 'magenta':
-            self.on_magenta()
-            return 'pass'
-        elif message.lower() == 'ciano':
-            self.on_cyan()
-            return 'pass'
-        elif message.lower() == 'branco':
-            self.on_white()
+    def set_color_by_name(self, color):
+        colors = {
+            'vermelho': (1, 0, 0),
+            'verde': (0, 1, 0),
+            'azul': (0, 0, 1),
+            'amarelo': (1, 1, 0),
+            'magenta': (1, 0, 1),
+            'ciano': (0, 1, 1),
+            'branco': (1, 1, 1)
+        }
+        
+        if color.lower() in colors:
+            self.set_color(*colors[color.lower()])
             return 'pass'
         else:
             return 'color_error'
     
-    def on_red(self):
-        self.on(1, 0, 0)
-    
-    def on_green(self):
-        self.on(0, 1, 0)
-    
-    def on_blue(self):
-        self.on(0, 0, 1)
-    
-    def on_yellow(self):
-        self.on(1, 1, 0)
-        
-    def on_magenta(self):
-        self.on(1, 0, 1)
-    
-    def on_cyan(self):
-        self.on(0, 1, 1)
-    
-    def on_white(self):
-        self.on(1, 1, 1)
-        
     def blink(self, red, green, blue, times, delay):
         for _ in range(times):
-            self.on(red, green, blue)
+            self.set_color(red, green, blue)
             time.sleep(delay)
             self.off()
             time.sleep(delay)
