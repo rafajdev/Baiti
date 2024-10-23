890f79f0dd69185b8936286d72f75f5b9c0929ab
RafaR <rafadeoliveiramendes@gmail.com>
2024-10-23 01:57:00 -0300

    Implemented camera suport and analysis

---
+++ src/functions/camera/control.py
@@ -0,0 +1,26 @@
+from .Camera import Camera
+from interaction.speech import *
+
+cam = Camera(1)
+
+def cam_request():
+   speak("Modo camera ativo!")
+   
+   while True:
+      speak("O que deseja fazer agora?")
+      user_response = listen()
+      
+      if user_response == 'voltar':
+         speak("Saindo do modo camera...")
+         return 'pass'
+      elif user_response == 'foto':
+         speak("Foto tirada!")
+         cam.take_picture()
+      elif user_response == 'analisar':
+         speak("O que quer que eu analise na imagem?")
+         result = cam.analyze_picture(listen())
+         
+         if result == 'no_image':
+            speak("Nenhuma imagem encontrada! Que tal tirar uma nova?")
+         else:
+            speak(result)
