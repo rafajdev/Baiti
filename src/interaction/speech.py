d67801642624e1b3c3d51821ac57a46c7f669c0b
RafaR <rafadeoliveiramendes@gmail.com>
2024-10-24 00:25:48 -0300

    Organized the try/catch block and added "Example" to function docstring

--- src/interaction/speech.py
+++ src/interaction/speech.py
@@ -10,33 +10,37 @@ def listen() -> str:
    and the recognize_google method to convert the audio to text. The language is set to pt-BR.
 
    Returns:
-      str: The text from the user's voice input, or None if an error occurred.
-   """
-   recognizer = speech_recognition.Recognizer()
-   with speech_recognition.Microphone() as source:
-      recognizer.adjust_for_ambient_noise(source, duration=0.5)
-      
-      beep(True)
-      print("Ouvindo...")   # Talvez será removido
-      
+      str: The transcribed text from the user's voice input.
 
-      try:
+   Example:
+      >>>
+      text_listened = listen()
+      print(text_listened)
+      # Output: "hello world"
+   """
+   try:
+      recognizer = speech_recognition.Recognizer()
+      with speech_recognition.Microphone() as source:
+         recognizer.adjust_for_ambient_noise(source, duration=0.5)
+         
+         beep(True)
+         print("Ouvindo...")   # Talvez ser  removido
+
          audio = recognizer.listen(source)
          text = recognizer.recognize_google(audio, language="pt-BR")
 
-         print(f"Você: {text}")
+         print(f"Voc : {text}")
          return text
-      
-      except (speech_recognition.UnknownValueError, speech_recognition.RequestError) as e:
-         print(f"Erro de escuta")
-         speak("Desculpe, não entendi... Pode repetir?")
-         return None
+   except (speech_recognition.UnknownValueError, speech_recognition.RequestError) as e:
+      print(f"Erro de escuta")
+      speak("Desculpe, n o entendi... Pode repetir?")
+      return None
 
-      except Exception as e:
-         print(f"Ocorreu um erro inesperado: {e}")
-         return None
+   except Exception as e:
+      print(f"Ocorreu um erro inesperado: {e}")
+      return None
            
-def speak(text):
+def speak(text: str):
    """
    Speak the given text by handling it and playing the audio.
    """
