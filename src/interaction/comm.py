01467a2a8103eab9fbb0ed4d54d17a5a255f25c3
RafaR <rafadeoliveiramendes@gmail.com>
2024-10-24 00:06:19 -0300

    Order of some statements changed

--- src/interaction/comm.py
+++ src/interaction/comm.py
@@ -8,10 +8,11 @@ def init():
    API = apiConfig()
    chat = API[0]
    
-   speak(json_h.read('standard_messages')['greetings']['pt-BR'])
    user_history = []
    model_history = []
-
+   
+   speak(json_h.read('standard_messages')['greetings']['pt-BR'])
+   
    while True:
       user_response = listen()
       algorithm_response = switch(user_response)
@@ -23,10 +24,12 @@ def init():
          continue
       
       chat_response = chat.send_message(algorithm_response)
+      
       user_history.append(user_response)
       model_history.append(chat_response.text)
+      
       print(chat_response.text)
       speak(chat_response.text)
 
-   # json_h.add_to_history('user', user_history)
-   # json_h.add_to_history('model', model_history)
+   #  json_h.add_to_history('user', user_history)
+   #  json_h.add_to_history('model', model_history)
